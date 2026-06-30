8dfe745b8a9ce9da3c95c6c85a61ba646a07c6c4
Tommaso Martone <t.martone@studenti.unisannio.it>
2026-06-30 14:32:42 +0200

    modifiche compreso per mobile

--- palestra.js/palestrahome.js
+++ palestra.js/palestrahome.js
@@ -1,3 +1,16 @@
 console.log("PowerGym online 💪");
 
 document.documentElement.style.scrollBehavior = "smooth";
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+});
+
+document.querySelectorAll(".price-card, .card, .info-box").forEach(el => {
+  el.classList.add("hidden");
+  observer.observe(el);
+});
